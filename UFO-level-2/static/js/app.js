39e3a300036737ba05c3f76e22c0f61b85293589
22bipp22 <tammibippert@gmail.com>
2020-10-13 00:30:26 -0500

    bonus work

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -38,7 +38,7 @@ function dataFilter() {
     //Select the html where the date will be entered
     // let valueInput = d3.event.target.value;
     // console.log(valueInput);
-
+    
     //Select the html where the date will be entered
     let inputDate = d3.select("#datetime");
     let inputCity = d3.select("#city");
@@ -57,16 +57,17 @@ function dataFilter() {
     //Determine which fields had values entered and save them to an array. 
     let filterKeys = [];
     let filterValues = [];
+    let newData = " ";
 
     if (dateValue) {
-        filterKeys.push("datetime");
-        filterValues.push(dateValue);
+        let newData = ufoData.filter(sighting => sighting.datetime === dateValue);
+        console.log(newData);
         
     };
 
     if (cityValue) {
-        filterKeys.push("city");
-        filterValues.push(cityValue);
+        let newData = newData.filter(sighting => sighting.city === cityValue);
+        console.log(newData);
         
     };
 
@@ -87,27 +88,14 @@ function dataFilter() {
         filterValues.push(shapeValue);
         
     };
-    
 
-    console.log(`keys ${filterKeys}, values ${filterValues}`);
-    //Use the date entered to filter the data
-    
-    let students = ['Leo', 'Adela', 'Juliet', 'Vanessa', 'Gina'];
+      //Use the date entered to filter the data
+    let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
 
-    //loop through the filter keys and filter the data. 
-    for (let x = 0; x < filterKeys.length; x ++) {
-        let searchItem = filterKeys[x];
-        let filteredData = ufoData.filter(sighting => sighting.searchItem === filterValues[x]);
-        console.log(searchItem);
-        console.log(filteredData);
-}
-
-    //let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
-
-    
-    // console.log(filteredData);
-
-    //Display only the filtered rows
+    // let filteredData = newData
+      // console.log(filteredData);
+  
+      //Display only the filtered rows
     let tbody = d3.select("tbody");
 
     //Clear the previously displayed data
@@ -115,12 +103,35 @@ function dataFilter() {
 
     //Populate the table area
     filteredData.forEach((foundDate) => {
-        // console.log(foundDate);
+        console.log(foundDate);
         let newRow = tbody.append('tr');
         Object.entries(foundDate).forEach(([key, value]) => {
             let newCell = newRow.append('td');
             newCell.text(value);
         });
     });
+};  
+    // console.log(`keys ${filterKeys}, values ${filterValues}`);
+    //Use the date entered to filter the data
 
-}
+   
+
+//     //loop through the filter keys and filter the data. 
+//     for (let x = 0; x < filterKeys.length; x ++) {
+//         let searchItem = filterKeys[x];
+//         let filteredData = ufoData.filter(ufoData.searchItem === filterValues[x]);
+//         console.log(ufoData.searchItem);
+//         console.log(filteredData);
+// }
+
+    //let filteredData = ufoData.filter(sighting => sighting.datetime === dateValue);
+
+    
+    // console.log(filteredData);
+
+
+// }
+// function letsFilter(item, value) {
+//     let newUfoData = ufoData.filter(sighting => sighting.datetime === value);
+//     console.log(newUfoData);
+// 
